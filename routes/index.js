c0e738bb2b16ffd7d141365fd9aeae95369b3a7c
Kerus Ashe <ashe@kerus.net>
2020-06-07 00:27:28 +0900

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const ejs = require('ejs');
+const cors = require('cors');
+
+const router = express.Router();
+
+
+const RequestManager = require(process.cwd() + '/lib/ResponseManager');
+
+router.get('/', cors(), (req, res) => {
+
+  res.render('index', {
+  });
+
+});
+
+router.get('/download', cors(), (req, res) => {
+
+	RequestManager.MakeProxyResponse(req, res);	
+
+});
+
+module.exports = router;
+
---
+++ web.js
@@ -0,0 +1,26 @@
+const CONFIG = require('./config.json');
+process.title = CONFIG.processTitle;
+
+const express = require('express');
+const routes = require('./routes/index');
+const path = require('path');
+const url = require('url');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('public'))
+app.use('/', routes);
+
+const server = app.listen(CONFIG.bindPortAddress, CONFIG.bindIPAddress, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
